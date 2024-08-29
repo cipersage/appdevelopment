044efbe94f76536f67ff1ec4459a5298c99396b2
cipersage <164124053+cipersage@users.noreply.github.com>
2024-08-29 09:46:01 +0530

    Add files via upload

---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,386 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button d1 , d2 , d3 , d4 ,d5 ,d6 , d7 ,d8 ,d9 , d0 , sin ,cos , tan , add , sub , mul ,div , sq , sqr ,fact , bac , bc , cb , ob ,ln ,log , inv ,pi , dot , eq;
+    String val , pii = "3.14159265";
+    double d;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        d1 = findViewById(R.id.d1);
+        d2 = findViewById(R.id.d2);
+        d3 = findViewById(R.id.d3);
+        d4 = findViewById(R.id.d4);
+        d5 = findViewById(R.id.d5);
+        d6 = findViewById(R.id.d6);
+        d7 = findViewById(R.id.d7);
+        d8 = findViewById(R.id.d8);
+        d9 = findViewById(R.id.d9);
+        d0 = findViewById(R.id.d0);
+        d1 = findViewById(R.id.d1);
+        sin = findViewById(R.id.sin);
+        cos = findViewById(R.id.cos);
+        tan = findViewById(R.id.tan);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+        sq = findViewById(R.id.sq);
+        sqr = findViewById(R.id.sqr);
+        fact = findViewById(R.id.fact);
+        bac = findViewById(R.id.bac);
+        bc = findViewById(R.id.bc);
+        cb = findViewById(R.id.cb);
+        ob = findViewById(R.id.ob);
+        ln = findViewById(R.id.ln);
+        log = findViewById(R.id.log);
+        inv = findViewById(R.id.inv);
+        pi = findViewById(R.id.pi);
+        dot = findViewById(R.id.dot);
+        eq = findViewById(R.id.eq);
+
+        TextView tvmain, tvsec;
+        tvmain = findViewById(R.id.tvmain);
+        tvsec = findViewById(R.id.tvsec);
+
+        // Adding event listner
+        d1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "1");
+            }
+        });
+
+        d2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "2");
+            }
+        });
+
+        d3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "3");
+            }
+        });
+
+
+        d4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "4");
+            }
+        });
+
+        d5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "5");
+            }
+        });
+
+        d6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "6");
+            }
+        });
+
+        d7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "7");
+            }
+        });
+
+        d8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "8");
+            }
+        });
+
+        d9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "9");
+            }
+        });
+
+        d0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "0");
+            }
+        });
+
+        sin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "sin");
+            }
+        });
+
+        cos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "cos");
+            }
+        });
+
+        tan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "tan");
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "+");
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "-");
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "x");
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "÷");
+            }
+        });
+
+        bac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvsec.setText("");
+                tvmain.setText("");
+            }
+        });
+
+        bc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String val = tvmain.getText().toString();
+                tvmain.setText(val.substring(0, val.length() - 1));
+            }
+        });
+
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + ")");
+            }
+        });
+
+        ob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "(");
+            }
+        });
+
+        ln.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "ln");
+            }
+        });
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "log");
+            }
+        });
+
+        inv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + "^" + "(-1)");
+            }
+        });
+
+        pi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + pii);
+            }
+        });
+
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setText(tvmain.getText() + ".");
+            }
+        });
+
+        eq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String val = tvmain.getText().toString();
+                String replacedstr = val.replace('÷', '/').replace('×', '*');
+                double result = eval(replacedstr);
+                tvmain.setText((String.valueOf(result)));
+                tvsec.setText(val);
+            }
+        });
+
+        sq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                val = tvmain.getText().toString();
+                d = Double.parseDouble(val);
+                tvmain.setText(d * d + "");
+                tvsec.setText(d + "²");
+            }
+        });
+
+
+        sqr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val = tvmain.getText().toString();
+                d = Math.sqrt(Double.parseDouble(val));
+                tvmain.setText(d + "");
+                tvsec.setText("√" + (d * d));
+            }
+        });
+
+        fact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tvmain.setText(tvmain.getText() + "!");
+
+            }
+        });
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    int factorial(int n) {
+        return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+    }
+    //eval function
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            //Grammer:
+            //expression = term | expression '+' term | expression '-' term
+            //term = factor | term '*' factor | term '/' factor
+            //factor = '+' factor | '-' factor | '(' expression ')'
+            //        | number | factorName factor | factor '^' factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm(); //Addition
+                    else if (eat('-')) x -= parseTerm(); //Subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseTerm(); //Multiplication
+                    else if (eat('/')) x /= parseTerm(); //Division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+
+                if (eat('+')) return parseFactor(); //Unary Plus
+                if (eat('-')) return -parseFactor(); //Unary Minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // Parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { //Numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if ((ch >= 'a' && ch <= 'z') || ch == '.') { //Functions
+                    while ((ch >= 'a' && ch <= 'z') || ch == '.') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else if (func.equals("log")) x = Math.log10(x);
+                    else if (func.equals("ln")) x = Math.log(x);
+                    else throw new RuntimeException("Unknown function: " + func);
+
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+                if (eat('^')) x = Math.pow(x, parseFactor()); //Exponentiation
+                return x;
+            }
+
+        }.parse();
+
+    }
+
+}
